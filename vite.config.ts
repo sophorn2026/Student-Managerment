77644e7ce3e42137f8bb7e885d39fc668b018832
sophorn2026 <290252860+sophorn2026@users.noreply.github.com>
2026-06-23 09:53:40 +0700

    chore: adjust base path for deployment
    Update vite configuration to set the base path dynamically based on the GITHUB_ACTIONS environment variable to support both local development and GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Student-Managerment/',
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/Student-Managerment/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
